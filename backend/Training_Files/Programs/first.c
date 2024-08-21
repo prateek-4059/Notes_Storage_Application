2db4395be02d4d7ffe3aa42b22cd77873dade5e3
prateek-4059 <129501539+prateek-4059@users.noreply.github.com>
2024-08-21 21:09:35 +0530

    Delete backend/Training_Files directory

--- backend/Training_Files/Programs/first.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("I love my India");
-    printf("\n\nDelhi is the capital of India.");
-    printf("\n\n\n");
-    printf("Lucknow\t\tKanpur\n\n\n\n\nVaranasi");
-}
