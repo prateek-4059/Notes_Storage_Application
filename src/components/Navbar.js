9b5f0cfa99be72fb89353c9fd89b1f64728c6302
prateek-4059 <129501539+prateek-4059@users.noreply.github.com>
2024-08-25 12:59:38 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
   
 
 const Navbar = () => {
     let location = useLocation();
+    let navigate=useNavigate();
+    const handleLogOut=()=>{
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
@@ -22,10 +27,10 @@ const Navbar = () => {
                         </li>
 
                     </ul>
-                    <form className="d-flex"> 
+                    {!localStorage.getItem('token')?<form className="d-flex"> 
                     <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
                     <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
-                    </form>
+                    </form>:<button onClick={handleLogOut} className='btn btn-primary'>Logout</button>}
                 </div>
             </div>
         </nav>
